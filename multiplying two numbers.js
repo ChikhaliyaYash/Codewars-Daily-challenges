3e72bba42f760dfe4e45079e16a9f79ef4008fd4
ChikhaliyaYash <chikhaliyayash899@gmail.com>
2022-08-26 23:13:13 +0530

    Done

---
+++ multiplying two numbers.js
@@ -0,0 +1,11 @@
+const multiply = (num1, num2) => {
+    return num1 * num2;
+  };
+  let Multiply = multiply(4, 5);
+  console.log(Multiply);
+
+// function multiply(x,y)
+// {
+//   return x*y;
+// }
+// console.log(multiply)
