f69f3b9355ed97234faba0fce111206de42ecdd4
ChikhaliyaYash <chikhaliyayash899@gmail.com>
2022-09-07 23:26:22 +0530

    Done

--- problem 8/Square(n) Sum.js
+++ problem 8/Square(n) Sum.js
@@ -4,3 +4,12 @@ let n = numbers;
       val+=n[i] **2
     }
     return val
+
+    // solution 2 ====>
+    // function squareSum(numbers){
+    //     var result = 0;
+    //     for (var i = 0; i < numbers.length; i++) {
+    //       result += (numbers[i] * numbers[i]);
+    //     }
+    //     return result;
+    //   }
