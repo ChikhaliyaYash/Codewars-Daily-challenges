ebaa7d8b8adca730189537cc0fb18820e2f3f0a2
ChikhaliyaYash <chikhaliyayash899@gmail.com>
2022-09-08 19:38:48 +0530

    Done

--- problem 10/Growth of a Population.js
+++ problem 10/Growth of a Population.js
@@ -1,10 +1,16 @@
 function nbYear(p0, percent, aug, p) {
-    // your code
-
- let year=0;
-   while (p0<p){
-     year++; 
-     p0=p0+(p0*(percent/100))+aug
-   }
-   return year
+    var count = 0;
+    while (p0 < p) {
+      p0 = Math.floor((1 + percent / 100) * p0 + aug);
+      count++
+    } 
+    return count;
 }
+
+// solution 2 ==>
+//  let year=0;
+//    while (p0<p){
+//      year++; 
+//      p0=p0+(p0*(percent/100))+aug
+//    }
+//    return year
